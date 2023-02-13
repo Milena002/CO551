11c0dbeb1ee7a507afb4ddacc756351a5e7de970
Milena002 <22143664@bucks.ac.uk>
2023-02-13 19:38:51 +0000

    wk3 ex2

---
+++ week3/wk3ex1action1.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+	<title>Response to form</title>
+</head>
+<body>
+</br> Your name is 
+<?php echo  $_POST['txtname'];?>
+</br> Your gender is
+<?php echo  $_POST['radsex'];?>
+</br>Your occupation is 
+<?php echo $_POST['seloccupation'];?>
+	
+
+</body>
+</html>
