e52c1a3392613eec4f01af1cbc19739f43e48919
Milena002 <22143664@bucks.ac.uk>
2023-02-14 14:09:20 +0000

    logbook wk 3 done

--- week1/wk1ex7.php
+++ week1/wk1ex7.php
@@ -4,9 +4,9 @@
 	$hourlyrate	= 5.75;
 	$hoursperweek = 40;
 	$gross = $hourlyrate * $hoursperweek;
-echo $gross;
-//$netwage = $gross * 0.78;
-//echo $netwage;
+//echo $gross;
+$netwage = $gross * 0.78;
+echo $netwage;
 ?>
 </body>
 </html>
