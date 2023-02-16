b0426b2f27e95db6ed9cb9d64d958a6b3ff45e34
Milena002 <22143664@bucks.ac.uk>
2023-02-16 22:39:33 +0000

    TEST

--- week4/confirmation.php
+++ week4/confirmation.php
@@ -1,15 +1,17 @@
-
 <html>
-  <head><title> Confirmation page</title></head>
+  <head><title>Select colour page</title></head>
     <body>
-        <h2> Your order qty is
-            <?php echo $_POST["selqty"] ?> 
-        widgets you are ordering </h2></br>
-   
-        <h2> And the selected colour is:
-            <?php echo $_POST["selcolour"] ?> 
-        . </h2></br>
-
+      <form action="confirmation.php"  method="post">
+	Select the colour for the <?php echo $_POST["selqty"] ?> widgets you are ordering
+   	<select name="selcolour">
+   	  <option>white</option>
+	  <option>red</option>
+	  <option>yellow</option>
+	  <option>green</option>
+	  <option>blue</option>
+        </select>
+        <br/><br/>	
+        <input type="submit" value="Buy"/>
+      </form>
    </body>
 </html>	
-
