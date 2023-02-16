71866bcb0cb5190a9fe9c918e5ee648509c10f2c
Milena002 <22143664@bucks.ac.uk>
2023-02-16 22:18:28 +0000

    week4

---
+++ week4/confirmation.php
@@ -0,0 +1,15 @@
+
+<html>
+  <head><title> Confirmation page</title></head>
+    <body>
+        <h2> Your order qty is
+            <?php echo $_POST["selqty"] ?> 
+        widgets you are ordering </h2></br>
+   
+        <h2> And the selected colour is:
+            <?php echo $_POST["selcolour"] ?> 
+        . </h2></br>
+
+   </body>
+</html>	
+
