7f76b62fff2f0429c654cc18af269252d1b6045a
Milena002 <22143664@bucks.ac.uk>
2023-03-25 13:02:25 +0000

    wk7

--- week7/wk7ex5.php
+++ week7/wk7ex5.php
@@ -4,7 +4,7 @@
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
 <?php
-    include("myfunctions.inc");
+    require"myfunctions.inc";
 
     if($_POST['btnSubmit'])
     {
